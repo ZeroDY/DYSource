b487a9ca93aa7ff20fd035fa718e835c94e2f293
ZeroDY <deyi233@gmail.com>
2016-06-03 00:59:30 +0800

    DYDataSourceDelegate

---
+++ DYDataSourceDelegate/DYTableViewControllerDataSource.h
@@ -0,0 +1,23 @@
+//
+//  DYTableViewControllerDataSource.h
+//  
+//
+//  Created by 周德艺 on 16/5/21.
+//  Copyright © 2016年 周德艺. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void (^TableViewCellConfigureBlock)(id cell, id item);
+
+@interface DYTableViewControllerDataSource : NSObject<UITableViewDataSource,UITableViewDelegate>
+
+@property (nonatomic, strong) NSArray *items;
+
+- (id)initWithItems:(NSArray *)anItems
+     cellIdentifier:(NSString *)aCellIdentifier
+ configureCellBlock:(TableViewCellConfigureBlock)aConfigureCellBlock;
+
+- (id)itemAtIndexPath:(NSIndexPath *)indexPath;
+
+@end
---
+++ README.md
@@ -0,0 +1,17 @@
+# 我的开发自用控件
+
+# DYSegment
+>分段选择器
+
+# DYMenuView
+>单级菜单栏
+
+# DYNetworking
+>网络请求框架。对AFNetworking 进行简单二次封装，学习了 [HYBNetworking](https://github.com/CoderJackyHuang/HYBNetworking) 
+
+# DYBannerView
+>滑动轮播控件
+
+# DYDataSourceDelegate
+>UITableView和 UICollectionView 的DataSource和Delegate
+
